3461a8176f1d5aabb3cf3d71ae713f87ad0f4b4d
psyworld <psygalaxia@gmail.com>
2018-03-12 04:03:21 +0300

    fisrt commit

---
+++ udp/udpClient.c
@@ -0,0 +1,64 @@
+#include <arpa/inet.h>
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> //memset
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define BUF_SIZE 512    //Max length of buffer
+#define PORT     8888   //The port on which to send data
+#define SERVER   "127.0.0.1"
+ 
+int main(void)
+{
+    struct sockaddr_in si_other;
+    int    s, i;
+    int    slen = sizeof(si_other);
+    char   buf[BUF_SIZE];
+    char   message[BUF_SIZE];
+ 
+
+    //create a socket
+    if ( (s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+ 
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port   = htons(PORT);
+     
+    if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+    {
+        perror("inet_aton()");
+        exit(1);
+    }
+ 
+    while(1)
+    {
+        printf("Enter message : ");
+        scanf("%s", message);
+         
+        //send the message
+        if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+        {
+            perror("sendto()");
+            exit(1);    
+        }
+                 
+        memset(buf,'\0', BUF_SIZE);
+        
+        //try to receive some data
+        if (recvfrom(s, buf, BUF_SIZE, 0, (struct sockaddr *) &si_other, &slen) == -1)
+        {
+            perror("recvfrom()");
+            exit(1);
+        }
+         
+        puts(buf);
+    }
+ 
+    close(s);
+    return 0;
+}
---
+++ udp/udpServer.c
@@ -0,0 +1,87 @@
+#include <stdio.h> 
+#include <string.h> //memset
+#include <stdlib.h> 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h> 
+
+#define BUF_SIZE 512    
+#define PORT     8888   
+
+int palindrome(char s[])
+{
+
+    for (int i = 0; i<strlen(s)/2;i++)
+    {
+        if (s[i] != s[strlen(s)-i-1])
+            return 0;
+    };
+
+    return 1;
+}
+ 
+int main(void)
+{
+    struct sockaddr_in si_me, si_other;
+     
+    int  s, i;
+    int  slen = sizeof(si_other) , recv_len;
+    char buf[BUF_SIZE];
+     
+    //create an UDP socket
+    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        perror("socket");
+        exit(1);
+    }
+     
+    //clear struct 
+    memset((char *) &si_me, 0, sizeof(si_me));
+     
+    si_me.sin_family = AF_INET;
+    si_me.sin_port = htons(PORT);
+    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+     
+    //bind socket to port
+    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
+    {
+        perror("bind");
+        exit(1);
+    }
+     
+    //listening 
+    while(1)
+    {
+
+        memset(buf, '\0', sizeof(buf));
+
+        printf("Waiting for data...");
+        fflush(stdout);
+         
+        //try to receive some data
+        if ((recv_len = recvfrom(s, buf, BUF_SIZE, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+        {
+            perror("recvfrom()");
+            exit(1);
+        }
+         
+        //print client details 
+        //printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        
+        if (palindrome(buf)) 
+            printf("\n%s is a palindrome.\n" , buf);
+        else 
+            printf("\n%s is not a palindrome.\n", buf);
+         
+        //reply the client
+        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
+        {
+            perror("sendto()");
+            exit(1);
+        }
+
+    }
+ 
+    close(s);
+    return 0;
+}
